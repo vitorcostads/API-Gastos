5ffbb9aa484aa3108ec917a002c0f36aefefd2b9
vitorcostads <vitor.costads@hotmail.com>
2025-10-08 10:40:25 -0300

    New main code

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from flask import Flask, request, jsonify
-import datetime
-
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/notificacaoS', methods=['POST'])
-def receber_notificacao():
-    data = request.json
-    mensagem = data.get("mensagem", "")
-    hora = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # Temporário: salva num log
-    with open("notificacoes_log.txt", "a", encoding="utf-8") as f:
-        f.write(f"[{hora}] {mensagem}\n")
-
-    print(f"Nova notificação: {mensagem}")
-    return jsonify({"status": "ok", "mensagem": mensagem}), 200
-
 @app.route('/')
 def home():
-    return "API Gastos está online!"
+    return "API Gastos online ✅"
 
-
-if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/notificacaos', methods=['POST'])
+def receber_notificacao():
+    data = request.json
+    print("Notificação recebida:", data)
+    return {"status": "ok"}, 200
