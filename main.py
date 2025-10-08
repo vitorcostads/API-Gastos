99b70b0cf6bc12720c1818096579bfb259f1e268
vitorcostads <vitor.costads@hotmail.com>
2025-10-08 12:29:51 -0300

    Main att and fix fly.toml

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import sys
+
+# Desativa buffering globalmente 
+sys.stdout.reconfigure(line_buffering=True)
 
 app = Flask(__name__)
 
@@ -8,6 +12,15 @@ def home():
 
 @app.route('/notificacaos', methods=['POST'])
 def receber_notificacao():
-    data = request.json
-    print("Notificação recebida:", data)
-    return {"status": "ok"}, 200
+    data = request.get_json(silent=True)  # evita erro se JSON malformado
+
+    if not data:
+        return jsonify({"erro": "Nenhum JSON válido recebido"}), 400
+
+    print("📩 Notificação recebida:", data, flush=True)  # imprime imediatamente
+
+    # Reenvia o mesmo JSON que recebeu para fins de teste
+    return jsonify({
+        "status": "ok",
+        "recebido": data
+    }), 200
